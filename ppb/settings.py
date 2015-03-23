e25f6a285479ade9ce97bacfbce772e8c40f6ee4
Patrick Altman <paltman@gmail.com>
2015-03-23 14:04:03 -0500

    Needs to have something to run local

--- ppb/settings.py
+++ ppb/settings.py
@@ -93,7 +93,7 @@ STATICFILES_FINDERS = [
 ]
 
 # Make this unique, and don"t share it with anybody.
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY", "fake")
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = [
