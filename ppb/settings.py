2633bf7c168d585c4e58dba960a487beec86b00a
Patrick Altman <paltman@gmail.com>
2015-03-23 13:44:01 -0500

    Hook up Gondor

--- ppb/settings.py
+++ ppb/settings.py
@@ -1,19 +1,46 @@
 import os
-
+import urlparse
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
 BASE_DIR = PACKAGE_ROOT
 
-DEBUG = True
+SITE_ID = int(os.environ.get("SITE_ID", 1))
+DEBUG = SITE_ID == 1
 TEMPLATE_DEBUG = DEBUG
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "ppb",
+if "GONDOR_DATABASE_URL" in os.environ:
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["GONDOR_DATABASE_URL"])
+    DATABASES = {
+        "default": {
+            "ENGINE": {
+                "postgres": "django.db.backends.postgresql_psycopg2"
+            }[url.scheme],
+            "NAME": url.path[1:],
+            "USER": url.username,
+            "PASSWORD": url.password,
+            "HOST": url.hostname,
+            "PORT": url.port
+        }
     }
-}
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql_psycopg2",
+            "NAME": "ppb",
+        }
+    }
+
+
+ALLOWED_HOSTS = [
+    "blog.pinaxproject.com"
+]
+
+if os.environ.get("GONDOR_INSTANCE_DOMAIN") is not None:
+    ALLOWED_HOSTS.append(
+        os.environ["GONDOR_INSTANCE_DOMAIN"]
+    )
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -22,13 +49,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = "UTC"
+TIME_ZONE = "US/Eastern"
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = "en-us"
-
-SITE_ID = int(os.environ.get("SITE_ID", 1))
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
@@ -41,23 +66,18 @@ USE_L10N = True
 # If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "media")
+MEDIA_ROOT = os.path.join(
+    os.environ.get("GONDOR_DATA_DIR", PACKAGE_ROOT),
+    "site_media",
+    "media"
+)
+STATIC_ROOT = os.path.join(
+    os.environ.get("GONDOR_DATA_DIR", PACKAGE_ROOT),
+    "site_media",
+    "static"
+)
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = "/site_media/media/"
-
-# Absolute path to the directory static files should be collected to.
-# Don"t put anything in this directory yourself; store your static files
-# in apps" "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PACKAGE_ROOT, "site_media", "static")
-
-# URL prefix for static files.
-# Example: "http://media.lawrence.com/static/"
 STATIC_URL = "/site_media/static/"
 
 # Additional locations of static files
@@ -73,7 +93,7 @@ STATICFILES_FINDERS = [
 ]
 
 # Make this unique, and don"t share it with anybody.
-SECRET_KEY = "i^1kykkqv4^i1!bl%aak-pjip!)j9wc00)9+!sw-v#t(_)jo_u"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = [
@@ -101,6 +121,7 @@ MIDDLEWARE_CLASSES = [
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "opbeat.contrib.django.middleware.OpbeatAPMMiddleware"
 ]
 
 ROOT_URLCONF = "ppb.urls"
@@ -128,10 +149,17 @@ INSTALLED_APPS = [
     # external
     "pinax.blog",
     "taggit",
+    "opbeat.contrib.django",
 
     # project
     "ppb",
 ]
+
+OPBEAT = {
+    "ORGANIZATION_ID": os.environ.get("OPBEAT_ORG_ID"),
+    "APP_ID": os.environ.get("OPBEAT_APP_ID"),
+    "SECRET_TOKEN": os.environ.get("OPBEAT_SECRET_TOKEN")
+}
 
 PINAX_BLOG_SECTIONS = [
     ("release-notes", "Release Notes")
@@ -170,4 +198,12 @@ FIXTURE_DIRS = [
     os.path.join(PROJECT_ROOT, "fixtures"),
 ]
 PINAX_BLOG_UNPUBLISHED_STATES = ["Raw", "Draft", "Reviewed"]
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+EMAIL_BACKEND = os.environ.get("EMAIL_BACKEND", "django.core.mail.backends.console.EmailBackend")
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "")
+EMAIL_PORT = os.environ.get("EMAIL_PORT", "")
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+EMAIL_USE_TLS = True
+
+DEFAULT_FROM_EMAIL = "developers@pinaxproject.com"
