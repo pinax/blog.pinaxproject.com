fdde3d45206d5e593a1e32041b58a714de56d06f
Jacob Wegner <jacobwegner@gmail.com>
2016-11-22 23:32:22 -0600

    drop gondor and add whitenoise, dj-database-url

--- ppb/settings.py
+++ ppb/settings.py
@@ -1,5 +1,6 @@
 import os
-import urlparse
+
+import dj_database_url
 
 PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
@@ -8,29 +9,9 @@ BASE_DIR = PACKAGE_ROOT
 SITE_ID = int(os.environ.get("SITE_ID", 1))
 DEBUG = SITE_ID == 1
 
-if "GONDOR_DATABASE_URL" in os.environ:
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(os.environ["GONDOR_DATABASE_URL"])
-    DATABASES = {
-        "default": {
-            "ENGINE": {
-                "postgres": "django.db.backends.postgresql_psycopg2"
-            }[url.scheme],
-            "NAME": url.path[1:],
-            "USER": url.username,
-            "PASSWORD": url.password,
-            "HOST": url.hostname,
-            "PORT": url.port
-        }
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql_psycopg2",
-            "NAME": "ppb",
-        }
-    }
-
+DATABASES = {
+    "default": dj_database_url.config(default="postgres://localhost/ppb")
+}
 
 ALLOWED_HOSTS = [
     "blog.pinaxproject.com"
@@ -121,6 +102,7 @@ TEMPLATES = [
 
 
 MIDDLEWARE_CLASSES = [
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
